2a49809f5e7ef79154568784b3b7ecaaf4ee58fe
Hajili <tu201806363@aztu.local>
2021-10-27 07:53:58 +0400

    elave tasklar

---
+++ tap3.cs
@@ -0,0 +1,14 @@
+   
+using System;
+
+class Program
+{
+    static void Main() 
+    {
+        Console.WriteLine("1.number: ");
+        int Number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("2.number: ");
+        int Number2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("total: "+(number1+number2));
+    }
+}
